2c6a8ab41eaf5d81b5d4f98fbe0cc55316fa6199
ian.dover <ian.dover@skunova.com>
2023-02-27 02:27:34 +0000

    seems that shape of graph is good

--- problem2/Problem2.py
+++ problem2/Problem2.py
@@ -120,19 +120,31 @@ def exact_line_search(
     k : float,
     c : float,
     y : np.ndarray,
-    gradient : np.ndarray,
-    learning_rate : float
+    gradient : np.ndarray
 ) -> float:
     '''
     '''
-    step_sizes = np.linspace(0.001, 1.0, num = 1000)
+    log_values = np.logspace(-10, 1, num = 10, base = 10.0)
+    incremental_values = np.arange(0.1, 1.1, 0.1)
+
+    step_sizes = np.outer(log_values, incremental_values).flatten()
+
+    exact_lines = np.array([
+        [
+            k + step_size * gradient[0],
+            c + step_size * gradient[1]
+        ]
+        for step_size in step_sizes
+    ])
+    step_sizes = step_sizes[(exact_lines >= 0).all(axis = 1)]
+    exact_lines = exact_lines[(exact_lines >= 0).all(axis = 1)]
 
     log_likelihoods = np.array([
         log_likelihood_function(
-            k + learning_rate * gradient[0], 
-            c + learning_rate * gradient[1],
+            exact_line[0], 
+            exact_line[1],
             y
-        ) for learning_rate in step_sizes
+        ) for exact_line in exact_lines
     ])
 
     best_step = step_sizes[np.argmax(log_likelihoods)]
@@ -140,13 +152,12 @@ def exact_line_search(
 
 def gradient_descent_using_exact_line_search(
     y : np.ndarray,
-    learning_rate : float,
     max_iterations : int
 ) -> tuple[float, float, np.ndarray, np.ndarray, np.ndarray]:
     '''
     '''
-    _k = 0.5
-    _c = 0.5
+    _k = 0.01
+    _c = 5.0
     
     k_history = []
     c_history = []
@@ -157,7 +168,7 @@ def gradient_descent_using_exact_line_search(
 
     for i in range(max_iterations):
         gradient = log_likelihood_gradient(_k, _c, y)
-        step_size = exact_line_search(_k, _c, y, gradient, learning_rate)
+        step_size = exact_line_search(_k, _c, y, gradient)
 
         _k = _k + step_size * gradient[0]
         k_history.append(_k)
@@ -178,9 +189,8 @@ if __name__ == '__main__':
     file_directory = os.path.abspath(os.path.join(current_path, '..', '..', 'data', 'Question2.csv'))
 
     y = np.genfromtxt(file_directory, delimiter = ',').flatten()
-    learning_rate = 1.0
-    max_iterations = 100
-    best_k, best_c, k_history, c_history, log_likelihood_history = gradient_descent_using_exact_line_search(y, learning_rate, max_iterations)
+    max_iterations = 10
+    best_k, best_c, k_history, c_history, log_likelihood_history = gradient_descent_using_exact_line_search(y, max_iterations)
 
     # Plot Log-Likelihood
     iterations = np.arange(0, len(log_likelihood_history))
@@ -192,5 +202,23 @@ if __name__ == '__main__':
 
     file_directory = os.path.abspath(os.path.join(current_path, '..', '..', 'output', 'gradient_ascent.png'))
     plt.savefig(file_directory, dpi = 100)
+
+    plt.clf()
+    plt.cla()
+
+    # Plot K and C History
+    iterations = np.arange(0, len(k_history))
+    sns.lineplot(x = iterations, y = k_history)
+    sns.lineplot(x = iterations, y = c_history)
+
+    plt.title('Values of K and C Using Gradient Ascent with Exact Line Search')
+    plt.xlabel('Iteration')
+    plt.ylabel('Parameter Values')
+
+    file_directory = os.path.abspath(os.path.join(current_path, '..', '..', 'output', 'k_and_c_history.png'))
+    plt.savefig(file_directory, dpi = 100)
+
+    plt.clf()
+    plt.cla()
 
     print(best_k, best_c)

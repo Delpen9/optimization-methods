7e6a9e665f5b49e24865699916545747e8c075a6
ian.dover <ian.dover@skunova.com>
2023-03-02 18:53:56 +0000

    included newtons method

--- problem2/newtons_method.py
+++ problem2/newtons_method.py
@@ -73,10 +73,64 @@ def log_likelihood_gradient(
     log_likelihood_gradient = np.array([_k_partial(k, c, y), _c_partial(k, c, y)])
     return log_likelihood_gradient
 
-def log_likelihood_hessian(
+def _partial_kk(
+    k : float,
+    c : float,
+    y : np.ndarray
+) -> float:
+    dkdk = np.sum(-1/k**2 * np.ones(y.shape))
+    return dkdk
 
+def _partial_ck(
+    k : float,
+    c : float,
+    y : np.ndarray
+) -> float:
+    numerator = -y**c * np.log(y)
+    denominator = y**c + 1
+    dcdk = np.sum(numerator / denominator)
+    return dcdk
+
+def _partial_kc(
+    k : float,
+    c : float,
+    y : np.ndarray
+) -> float:
+    numerator = -y**c * np.log(y)
+    denominator = y**c + 1
+    dkdc = np.sum(numerator / denominator)
+    return dkdc
+
+def _partial_cc(
+    k : float,
+    c : float,
+    y : np.ndarray
+) -> float:
+    term_one = -1/c**2
+
+    term_two_constant = -(k + 1)
+    term_two = y**c*np.log(y)**2 / (y**c + 1) -\
+            y**(2*c)*np.log(y)**2 / (y**c + 1)**2
+    term_two *= term_two_constant
+
+    dcdc = np.sum(term_one + term_two)
+    return dcdc
+
+def log_likelihood_hessian(
+    k : float,
+    c : float,
+    y : np.ndarray
 ) -> np.ndarray:
-    return None
+    dkdk = _partial_kk(k, c, y)
+    dkdc = _partial_kc(k, c, y)
+    dcdk = _partial_ck(k, c, y)
+    dcdc = _partial_cc(k, c, y)
+
+    hessian = np.array([
+        [dkdk, dkdc],
+        [dcdk, dcdc]
+    ])
+    return hessian
 
 def log_likelihood_function(
     k : float,
@@ -103,3 +157,79 @@ def log_likelihood_function(
     )
 
     return log_likelihood
+
+def exact_line_search(
+    k : float,
+    c : float,
+    y : np.ndarray,
+    gradient : np.ndarray
+) -> np.ndarray[float, float]:
+    '''
+    '''
+    log_values = np.logspace(-2, 2, num = 4, base = 10.0)
+    incremental_values = np.arange(0.2, 1.01, 0.2)
+
+    step_sizes = np.outer(log_values, incremental_values).flatten()
+    X, Y = np.meshgrid(step_sizes, step_sizes)
+    meshed_step_sizes = np.stack((X, Y), axis = -1)
+    meshed_step_sizes = meshed_step_sizes.reshape(len(step_sizes)**2, 2)
+
+    exact_lines = np.array([
+        [
+            k + meshed_step_size[0] * gradient[0],
+            c + meshed_step_size[1] * gradient[1]
+        ]
+        for meshed_step_size in meshed_step_sizes
+    ])
+    meshed_step_sizes = meshed_step_sizes[(exact_lines >= 0).all(axis = 1)]
+    exact_lines = exact_lines[(exact_lines >= 0).all(axis = 1)]
+
+    log_likelihoods = np.array([
+        log_likelihood_function(
+            exact_line[0], 
+            exact_line[1],
+            y
+        ) for exact_line in exact_lines
+    ])
+
+    best_step = meshed_step_sizes[np.argmax(log_likelihoods)]
+    return best_step
+
+def newtons_method_using_exact_line_search(
+    y : np.ndarray,
+    tolerance : float = 1e-2
+) -> tuple[float, float, np.ndarray, np.ndarray, np.ndarray]:
+    '''
+    '''
+    _k = 0.5
+    _c = 0.5
+    
+    k_history = []
+    k_history.append(_k)
+    c_history = []
+    c_history.append(_c)
+    log_likelihood_history = []
+
+    log_likelihood = log_likelihood_function(_k, _c, y)
+    log_likelihood_history.append(log_likelihood)
+
+    gradient = log_likelihood_gradient(_k, _c, y)
+
+    while (np.dot(gradient.T, gradient) > tolerance):
+        gradient = log_likelihood_gradient(_k, _c, y)
+        meshed_step_size = exact_line_search(_k, _c, y, gradient)
+        
+        _k = _k + meshed_step_size[0] * gradient[0]
+        k_history.append(_k)
+
+        _c = _c + meshed_step_size[1] * gradient[1]
+        c_history.append(_c)
+
+        log_likelihood_old = log_likelihood
+        log_likelihood = log_likelihood_function(_k, _c, y)
+
+        log_likelihood_history.append(log_likelihood)
+
+    best_k = _k
+    best_c = _c
+    return (best_k, best_c, k_history, c_history, log_likelihood_history)
